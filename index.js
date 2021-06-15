22a88ac8412e8d9d57414c90b8bc594fb57631e9
olawaleKareemDev <olawalekareemdev@gmail.com>
2021-06-15 18:25:11 +0100

    The first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports.biometrics = (name) => console.log(`your name is ${name}`);
+
+module.exports.addition = (a, b) => console.log(a + b);
+module.exports.multiplication = (a, b) => console.log(a * b);
+module.exports.subtraction = (a, b) => console.log(a - b);
